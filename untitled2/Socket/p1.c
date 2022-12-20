1b0f77ff035a3dcb70869dd16661422e45e85b8c
sakshamp10 <saksham21486@iiitd.ac.in>
2022-12-20 21:13:24 +0530

    second commit

--- untitled2/Socket/p1.c
+++ untitled2/Socket/p1.c
@@ -1,3 +1,212 @@
+//
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//#include<time.h>
+//#include<sys/stat.h>
+//#include<sys/types.h>
+//#include<fcntl.h>
+//#include<errno.h>
+//#include<unistd.h>
+//#include<sys/socket.h>
+//#include<sys/un.h>
+//
+//#define MAX_MESSAGE_SIZE 6
+//#define len 6
+//#define num 50
+//
+//char* LOCAL = "./sockP1";
+//char* DESTINATION = "./sockP2";
+//
+//int min(int a , int b){
+//    if(a>b) return b;
+//    else return a;
+//}
+//
+//struct myStruct{
+//    char* myIdx;
+//    char* myStr;
+//};
+//
+//int idxsize(int i){
+//    if(i>=10){
+//        return 3;
+//    }
+//    else return 2;
+//}
+//
+//void int_to_char(int curr, char **ptr){
+//    if(curr>=10){
+//        (*ptr)[0] = '0'+curr/10;
+//        (*ptr)[1] = '0'+curr%10;
+//        (*ptr)[2] ='\0';
+//    }
+//    else{
+//        (*ptr)[0] = '0'+curr;
+//        (*ptr)[1] = '\0';
+//    }
+//}
+//
+//
+//void generate_n_rand_str(struct myStruct** myData){
+//    srand(time(NULL));
+//    *myData = (struct myStruct*) malloc(num*sizeof(struct myStruct));
+//    int curr = 0;
+//    for(curr=0;curr<num;curr++)
+//    {
+//        (*myData)[curr].myStr = (char*) malloc((len)*sizeof(char));
+//        (*myData)[curr].myIdx = (char*) malloc(idxsize(curr)*sizeof(char));
+////        int_to_char(curr, &(*myData)[curr].myIdx);
+//
+//        if(curr>=10){
+//            (*myData)[curr].myIdx[0] = '0'+curr/10;
+//            (*myData)[curr].myIdx[1] = '0'+curr%10;
+//            (*myData)[curr].myIdx[2] ='\0';
+//        }
+//        else{
+//            (*myData)[curr].myIdx[0] = '0'+curr;
+//            (*myData)[curr].myIdx[1] = '\0';
+//        }
+//
+//
+//        int i=0;
+//        while(i<len-1)
+//        {
+//            (*myData)[curr].myStr[i] = 65 + rand()%26;
+//            i++;
+//        }
+//        (*myData)[curr].myStr[len-1] = '\0';
+////        curr++;
+//    }
+//}
+//
+//void send_t_rand_str(struct myStruct* myData, int t, int* start){
+//    struct sockaddr_un address;
+//    int fd;
+//
+//    address.sun_family = AF_UNIX;
+//    memcpy(address.sun_path, LOCAL, strlen(LOCAL) + 1);
+//
+//    if((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1){
+//        perror("Socket cannot be initialized!1");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    unlink(LOCAL);
+//    if(bind(fd, (struct sockaddr*) &address, sizeof(address)) == 0){}
+//    else{
+//        perror("Socket cannot be bound!");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    struct sockaddr_un destination;
+//    destination.sun_family = AF_UNIX;
+//    memcpy(destination.sun_path, DESTINATION, strlen(DESTINATION) + 1);
+//    int i=*start;
+//    while(i<min(*start+t,num))
+//    {
+//        // printf("Write a message: ");
+//        sendto(fd, myData[i].myIdx, (idxsize(i)), 0, (struct sockaddr*) &destination, sizeof(destination));
+//        sendto(fd, myData[i].myStr, len, 0, (struct sockaddr*) &destination, sizeof(destination));
+//        // connection_fd is marked as connected
+//        // and it knows where the message should be directed
+//        printf("%s %s\n", myData[i].myIdx, myData[i].myStr);
+//        i++;
+//    }
+//
+//    close(fd);
+//}
+//
+//int receive_last_rand_str(struct myStruct** myData, int *start){
+//    printf("%d",1);
+//    struct sockaddr_un address;
+//    int fd;
+//    fd = socket(AF_UNIX, SOCK_DGRAM, 0);
+//    if(fd != -1){}
+//    else{
+//        printf("Socket cannot be initialized!\n");
+//        exit(EXIT_FAILURE);
+//    }
+//    printf("%d",2);
+//    address.sun_family = AF_UNIX;
+//    memcpy(address.sun_path, LOCAL, strlen(LOCAL) + 1);
+//
+//    unlink(LOCAL);
+//    if(bind(fd, (struct sockaddr*) &address, sizeof(address)) == 0){}
+//    else{
+//        perror("Socket cannot be bound!");
+//        exit(EXIT_FAILURE);
+//    }
+//
+//    printf("%d",3);
+//
+//
+//    struct sockaddr_un emitter;
+//    socklen_t length;
+//
+//    struct myStruct *temp = (struct myStruct*) malloc(sizeof(struct myStruct));
+//    temp->myIdx = (char*) malloc(sizeof(char)*(idxsize(*start)));
+//    temp->myStr = (char*) malloc(sizeof(char)*MAX_MESSAGE_SIZE);
+//    size_t size;
+//
+//    printf("%d",4);
+//
+//    // printf("Write a message: ");
+//    size = recvfrom(fd, temp->myIdx, (idxsize(*start)), 0, (struct sockaddr *) &emitter, &length);
+//    if(size == -1) {
+//        if(errno == ECONNRESET) printf("ECONNRESET\n");
+//        close(fd);
+//        perror("Receiver"); exit(EXIT_FAILURE);
+//    }
+//    printf("%d",5);
+//
+//    size = recvfrom(fd, temp->myStr, len, 0, (struct sockaddr *) &emitter, &length);
+//    if(size == -1) {
+//        if(errno == ECONNRESET) printf("ECONNRESET\n");
+//        close(fd);
+//        perror("Receiver"); exit(EXIT_FAILURE);
+//    }
+//    printf("%d",6);
+//
+//    // connection_fd is marked as connected
+//    // and it knows where the message should be directed
+//    printf("%s\n", temp->myIdx);
+//    int ans=0;
+//    int i = 0;
+//    while(temp->myIdx[i]!='\0'){
+//        ans *= 10;
+//        ans += (int)((temp->myIdx[i]-'0'));
+//        i++;
+//    }
+//    int r_val=ans;
+//    free(temp);
+//    close(fd);
+//
+//    printf("%d",7);
+//
+//    return r_val;
+//}
+//
+//int main(int argc, const char* argv[]){
+//    struct myStruct* myData;
+//
+//    generate_n_rand_str(&myData) ;
+//
+//    int start = 0;
+//    while(start<num){
+//        printf("Sent data:\n");
+//        send_t_rand_str(myData, 5, &start);
+//        printf("----------------\n");
+//        printf("Received data:\n");
+//        printf("Start\n");
+//        start = receive_last_rand_str(&myData, &start);
+//        start++;
+//        printf("----------------\n");
+//        sleep(1);
+//    }
+//
+//    return 0;
+//}
 
 #include<stdio.h>
 #include<stdlib.h>
@@ -11,29 +220,17 @@
 #include<sys/socket.h>
 #include<sys/un.h>
 
+#define PENDING_QUEUE_SIZE 1
 #define MAX_MESSAGE_SIZE 6
-#define len 6
-#define num 50
+#define LOCAL       "./sockP1"
+#define DESTINATION "./sockP2"
 
-char* LOCAL = "./sockP1";
-char* DESTINATION = "./sockP2";
-
-int min(int a , int b){
-    if(a>b) return b;
-    else return a;
-}
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 struct myStruct{
     char* myIdx;
     char* myStr;
 };
-
-int idxsize(int i){
-    if(i>=10){
-        return 3;
-    }
-    else return 2;
-}
 
 void int_to_char(int curr, char **ptr){
     if(curr>=10){
@@ -47,40 +244,35 @@ void int_to_char(int curr, char **ptr){
     }
 }
 
+int char_to_int(char** ptr){
+    int ans=0;
+    int i = 0;
+    while((*ptr)[i]!='\0'){
+        ans *= 10;
+        ans += (int)((*ptr)[i]-'0');
+        i++;
+    }
+    return ans;
+}
 
-void generate_n_rand_str(struct myStruct** myData){
+void generate_n_rand_str(struct myStruct** myData, int n, int l){
     srand(time(NULL));
-    *myData = (struct myStruct*) malloc(num*sizeof(struct myStruct));
+    *myData = (struct myStruct*) malloc(n*sizeof(struct myStruct));
     int curr = 0;
-    for(curr=0;curr<num;curr++)
-    {
-        (*myData)[curr].myStr = (char*) malloc((len)*sizeof(char));
-        (*myData)[curr].myIdx = (char*) malloc(idxsize(curr)*sizeof(char));
-//        int_to_char(curr, &(*myData)[curr].myIdx);
+    while(curr<n){
+        (*myData)[curr].myStr = (char*) malloc((l)*sizeof(char));
+        (*myData)[curr].myIdx = (char*) malloc((curr<10?2:3)*sizeof(char));
+        int_to_char(curr, &(*myData)[curr].myIdx);
 
-        if(curr>=10){
-            (*myData)[curr].myIdx[0] = '0'+curr/10;
-            (*myData)[curr].myIdx[1] = '0'+curr%10;
-            (*myData)[curr].myIdx[2] ='\0';
+        for(int i = 0; i <= l-2; i++){
+            (*myData)[curr].myStr[i] = 33 + rand()%62;
         }
-        else{
-            (*myData)[curr].myIdx[0] = '0'+curr;
-            (*myData)[curr].myIdx[1] = '\0';
-        }
-
-
-        int i=0;
-        while(i<len-1)
-        {
-            (*myData)[curr].myStr[i] = 65 + rand()%26;
-            i++;
-        }
-        (*myData)[curr].myStr[len-1] = '\0';
-//        curr++;
+        (*myData)[curr].myStr[l-1] = '\0';
+        curr++;
     }
 }
 
-void send_t_rand_str(struct myStruct* myData, int t, int* start){
+void send_t_rand_str(struct myStruct* myData, int n, int l, int t, int* start){
     struct sockaddr_un address;
     int fd;
 
@@ -88,13 +280,12 @@ void send_t_rand_str(struct myStruct* myData, int t, int* start){
     memcpy(address.sun_path, LOCAL, strlen(LOCAL) + 1);
 
     if((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1){
-        perror("Socket cannot be initialized!1");
+        perror("Socket cannot be initialized!");
         exit(EXIT_FAILURE);
     }
 
     unlink(LOCAL);
-    if(bind(fd, (struct sockaddr*) &address, sizeof(address)) == 0){}
-    else{
+    if(bind(fd, (struct sockaddr*) &address, sizeof(address)) == -1){
         perror("Socket cannot be bound!");
         exit(EXIT_FAILURE);
     }
@@ -102,105 +293,86 @@ void send_t_rand_str(struct myStruct* myData, int t, int* start){
     struct sockaddr_un destination;
     destination.sun_family = AF_UNIX;
     memcpy(destination.sun_path, DESTINATION, strlen(DESTINATION) + 1);
-    int i=*start;
-    while(i<min(*start+t,num))
-    {
+
+    for(int i=*start; i<MIN(*start+t,n); i++){
         // printf("Write a message: ");
-        sendto(fd, myData[i].myIdx, (idxsize(i)), 0, (struct sockaddr*) &destination, sizeof(destination));
-        sendto(fd, myData[i].myStr, len, 0, (struct sockaddr*) &destination, sizeof(destination));
+        sendto(fd, myData[i].myIdx, (i<10?2:3), 0, (struct sockaddr*) &destination, sizeof(destination));
+        sendto(fd, myData[i].myStr, l, 0, (struct sockaddr*) &destination, sizeof(destination));
         // connection_fd is marked as connected
         // and it knows where the message should be directed
         printf("%s %s\n", myData[i].myIdx, myData[i].myStr);
-        i++;
     }
 
     close(fd);
 }
 
-int receive_last_rand_str(struct myStruct** myData, int *start){
-    printf("%d",1);
+int receive_last_rand_str(struct myStruct** myData, int n, int l, int *start){
     struct sockaddr_un address;
     int fd;
-    fd = socket(AF_UNIX, SOCK_DGRAM, 0);
-    if(fd != -1){}
-    else{
-        printf("Socket cannot be initialized!\n");
+
+    if((fd = socket(AF_UNIX, SOCK_DGRAM, 0)) == -1){
+        perror("Socket cannot be initialized!");
         exit(EXIT_FAILURE);
     }
-    printf("%d",2);
+
     address.sun_family = AF_UNIX;
     memcpy(address.sun_path, LOCAL, strlen(LOCAL) + 1);
 
     unlink(LOCAL);
-    if(bind(fd, (struct sockaddr*) &address, sizeof(address)) == 0){}
-    else{
+    if(bind(fd, (struct sockaddr*) &address, sizeof(address)) == -1){
         perror("Socket cannot be bound!");
         exit(EXIT_FAILURE);
     }
 
-    printf("%d",3);
-
-
     struct sockaddr_un emitter;
-    socklen_t length;
+    socklen_t len;
 
     struct myStruct *temp = (struct myStruct*) malloc(sizeof(struct myStruct));
-    temp->myIdx = (char*) malloc(sizeof(char)*(idxsize(*start)));
+    temp->myIdx = (char*) malloc(sizeof(char)*((*start)<10?2:3));
     temp->myStr = (char*) malloc(sizeof(char)*MAX_MESSAGE_SIZE);
     size_t size;
-
-    printf("%d",4);
-
     // printf("Write a message: ");
-    size = recvfrom(fd, temp->myIdx, (idxsize(*start)), 0, (struct sockaddr *) &emitter, &length);
+    size = recvfrom(fd, temp->myIdx, ((*start)<10?2:3), 0, (struct sockaddr *) &emitter, &len);
     if(size == -1) {
         if(errno == ECONNRESET) printf("ECONNRESET\n");
         close(fd);
         perror("Receiver"); exit(EXIT_FAILURE);
     }
-    printf("%d",5);
-
-    size = recvfrom(fd, temp->myStr, len, 0, (struct sockaddr *) &emitter, &length);
+    size = recvfrom(fd, temp->myStr, l, 0, (struct sockaddr *) &emitter, &len);
     if(size == -1) {
         if(errno == ECONNRESET) printf("ECONNRESET\n");
         close(fd);
         perror("Receiver"); exit(EXIT_FAILURE);
     }
-    printf("%d",6);
-
     // connection_fd is marked as connected
     // and it knows where the message should be directed
     printf("%s\n", temp->myIdx);
-    int ans=0;
-    int i = 0;
-    while(temp->myIdx[i]!='\0'){
-        ans *= 10;
-        ans += (int)((temp->myIdx[i]-'0'));
-        i++;
-    }
-    int r_val=ans;
+    // printf("%s %s\n", temp->myIdx, temp->myStr);
+
+    int r_val = char_to_int(&temp->myIdx);
     free(temp);
     close(fd);
-
-    printf("%d",7);
-
     return r_val;
 }
 
 int main(int argc, const char* argv[]){
     struct myStruct* myData;
-
-    generate_n_rand_str(&myData) ;
+    int num_of_rand_str = 50;
+    int len_of_rand_str = 6;
+    generate_n_rand_str(&myData, num_of_rand_str, len_of_rand_str) ;
+    // printf("ok\n");
+    // for(int i=0; i<num_of_rand_str; i++){
+    //     printf("%s %s\n", myData[i].myIdx, myData[i].myStr);
+    // }
+    // printf("ok\n");
 
     int start = 0;
-    while(start<num){
+    while(start<num_of_rand_str){
         printf("Sent data:\n");
-        send_t_rand_str(myData, 5, &start);
+        send_t_rand_str(myData, num_of_rand_str, len_of_rand_str, 5, &start);
         printf("----------------\n");
         printf("Received data:\n");
-        printf("Start\n");
-        start = receive_last_rand_str(&myData, &start);
-        start++;
+        start = receive_last_rand_str(&myData, num_of_rand_str, len_of_rand_str, &start)+1;
         printf("----------------\n");
         sleep(1);
     }
