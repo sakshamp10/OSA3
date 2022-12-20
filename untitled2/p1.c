f1839713ff836a2416fcdc07f1ad753ee3ffe331
sakshamp10 <saksham21486@iiitd.ac.in>
2022-12-20 18:56:48 +0530

    second commit

--- untitled2/p1.c
+++ untitled2/p1.c
@@ -8,14 +8,24 @@
 #include<errno.h>
 #include<unistd.h>
 
-#define PIPE_NAME "./myFifo"
+#define len 6
+#define num 50
+char* FIFO="./fifo";
 
-#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 struct myStruct{
     char* myIdx;
     char* myStr;
 };
+
+int min(int a, int b){
+    if(a>b){
+        return b;
+    }
+    else{
+        return a;
+    }
+}
 
 void int_to_char(int curr, char **ptr){
     if(curr>=10){
@@ -40,73 +50,66 @@ int char_to_int(char** ptr){
     return ans;
 }
 
-void generate_n_rand_str(struct myStruct** myData, int n, int l){
+void generate_n_rand_str(struct myStruct** myData){
     srand(time(NULL));
-    *myData = (struct myStruct*) malloc(n*sizeof(struct myStruct));
+    *myData = (struct myStruct*) malloc(num*sizeof(struct myStruct));
     int curr = 0;
-    while(curr<n){
-        (*myData)[curr].myStr = (char*) malloc((l)*sizeof(char));
+    for(;curr<num;curr++)
+    {
+        (*myData)[curr].myStr = (char*) malloc((len)*sizeof(char));
         (*myData)[curr].myIdx = (char*) malloc((curr<10?2:3)*sizeof(char));
         int_to_char(curr, &(*myData)[curr].myIdx);
 
-        for(int i = 0; i <= l-2; i++){
+        for(int i = 0; i <= len-2; i++){
             (*myData)[curr].myStr[i] = 33 + rand()%62;
         }
-        (*myData)[curr].myStr[l-1] = '\0';
-        curr++;
+        (*myData)[curr].myStr[len-1] = '\0';
     }
 }
 
-void send_t_rand_str(struct myStruct* myData, int n, int l, int t, int* start){
+void send_t_rand_str(struct myStruct* myData, int t, int* start){
     // Creating FIFO
     umask(0);
-    if(mknod(PIPE_NAME, S_IFIFO | 0660, 0) == -1){
+    if(mknod(FIFO, S_IFIFO | 0660, 0) == -1){
         if(errno != EEXIST){
             perror("Cannot create FIFO");
             exit(EXIT_FAILURE);
         }
-        // else{
-        // 	printf("Using existing FIFO\n");
-        // }
+
     }
     else{
         printf("FIFO created successfully\n");
     }
-    // printf("%d %d \n", *start, t);
     // Writing
-    int fd = open(PIPE_NAME, O_WRONLY);
-    for(int i=*start; i<MIN(*start+t,n); i++){
+    int fd = open(FIFO, O_WRONLY);
+    for(int i=*start; i<min(*start+t,num); i++){
         printf("%s %s\n", myData[i].myIdx, myData[i].myStr);
         write(fd, myData[i].myIdx, (i<10?2:3));
-        write(fd, myData[i].myStr, l);
+        write(fd, myData[i].myStr, len);
     }
     close(fd);
 }
 
-int receive_last_rand_str(struct myStruct** myData, int n, int l, int *start){
+int receive_last_rand_str(struct myStruct** myData, int *start){
     struct myStruct* temp = (struct myStruct*) malloc(sizeof(struct myStruct));
     temp->myIdx = (char*) malloc(((*start-1)<10?2:3)*sizeof(char));
-    temp->myStr = (char*) malloc(l*sizeof(char));
+    temp->myStr = (char*) malloc(len*sizeof(char));
     // Creating FIFO
     umask(0);
-    if(mknod(PIPE_NAME, S_IFIFO | 0660, 0) == -1){
+    if(mknod(FIFO, S_IFIFO | 0660, 0) == -1){
         if(errno != EEXIST){
-            perror("Cannot create FIFO");
+            printf("ERROR CREATING FIFO");
             exit(EXIT_FAILURE);
         }
-        // else{
-        // 	printf("Using existing FIFO\n");
-        // }
     }
     else{
         printf("FIFO created successfully\n");
     }
-    // printf("%d %d \n", *start, t);
+
     // Reading
-    int fd = open(PIPE_NAME, O_RDONLY);
+    int fd = open(FIFO, O_RDONLY);
     read(fd, temp->myIdx, ((*start-1)<10?2:3));
-    read(fd, temp->myStr, l);
-    // printf("%s %s\n", temp->myIdx, temp->myStr);
+    read(fd, temp->myStr, len);
     printf("%s\n", temp->myIdx);
     close(fd);
     return char_to_int(&temp->myIdx);
@@ -114,23 +117,17 @@ int receive_last_rand_str(struct myStruct** myData, int n, int l, int *start){
 
 int main(int argc, const char* argv[]){
     struct myStruct* myData;
-    int num_of_rand_str = 50;
-    int len_of_rand_str = 6;
-    generate_n_rand_str(&myData, num_of_rand_str, len_of_rand_str) ;
-    // printf("ok\n");
-    // for(int i=0; i<num_of_rand_str; i++){
-    //     printf("%s %s\n", myData[i].myIdx, myData[i].myStr);
-    // }
-    // printf("ok\n");
+    generate_n_rand_str(&myData) ;
 
     int start = 0;
-    while(start<num_of_rand_str){
+    while(start<num){
         printf("Sent data(1):\n");
-        send_t_rand_str(myData, num_of_rand_str, len_of_rand_str, 5, &start);
+        send_t_rand_str(myData, 5, &start);
         printf("----------------\n");
         sleep(1);
         printf("Received data(1):\n");
-        start = receive_last_rand_str(&myData, num_of_rand_str, len_of_rand_str, &start)+1;
+        start = receive_last_rand_str(&myData, &start);
+        start++;
         printf("----------------\n");
         sleep(0.5);
     }
